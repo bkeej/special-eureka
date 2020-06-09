e84a663e6a191f9bb8d15516db755bd5471877e3
Robert Henderson <balamkej@gmail.com>
2020-06-09 14:27:18 -0700

    igtxml-to-morph_csv.py

---
+++ data/igtxml-to-morph_csv.py
@@ -0,0 +1,15 @@
+import os
+import csv 
+import xml.etree.ElementTree as ET 
+
+#
+# Files and Directories
+#
+
+corpus_dir = "uspanteko_corpus_xml/"
+
+xml_files = os.listdir(corpus_dir)
+
+#
+# Parsers
+#
