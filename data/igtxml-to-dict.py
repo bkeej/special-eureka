9733ce6d95f00f2f21f2d28a1be52391de0cc3d5
Robert Henderson <balamkej@gmail.com>
2020-06-10 15:58:39 -0700

    zip morphs and glosses

--- data/igtxml-to-dict.py
+++ data/igtxml-to-dict.py
@@ -10,7 +10,32 @@ corpus_dir = 'uspanteko_corpus_xml/'
 
 xml_files = os.listdir(corpus_dir)
 
-
 #
 # Parsers
 #
+
+def get_morph_gloss(xmlfile):
+	tree = ET.parse(corpus_dir + xmlfile)
+	root = tree.getroot()
+
+	morph_gloss = []
+
+	for phrase in root.findall('./body/morphemes/phrase'):
+		try:
+			morph_list = phrase.findall('morph')
+			gloss_list = root.find("./body/gloss/phrase[@phrase_ref='" + phrase.get('phrase_ref') + "']").findall('gls')
+
+			morph_gloss = morph_gloss + list(zip(morph_list,gloss_list))
+		except AttributeError:
+			continue
+
+	return morph_gloss
+
+
+def main():
+
+	# print(len(get_gloss('1.xml')))
+	print(get_morph_gloss('3.xml'))
+
+if __name__ == '__main__':
+	main() 
