ca910b21b6276ae32a301aa72e6b22eca9a24b01
Robert Henderson <balamkej@gmail.com>
2020-06-24 09:37:12 -0700

    finished script to extract pos data at phrase level

--- data/igtxml-to-pos-list.py
+++ data/igtxml-to-pos-list.py
@@ -14,23 +14,6 @@ xml_files = os.listdir(corpus_dir)
 # Parsers
 #
 
-# Can't use this because the gls section of this corpus is fucked.
-# def get_morph_gloss(xmlfile):
-# 	tree = ET.parse(corpus_dir + xmlfile)
-# 	root = tree.getroot()
-
-# 	morph_gloss = []
-
-# 	for phrase in root.findall('./body/morphemes/phrase'):
-# 		try:
-# 			morph_list = phrase.findall('morph')
-# 			gloss_list = root.find("./body/gloss/phrase[@phrase_ref='" + phrase.get('phrase_ref') + "']").findall('gls')
-
-# 			morph_gloss = morph_gloss + list(zip(morph_list,gloss_list))
-# 		except AttributeError:
-# 			continue
-
-# 	return morph_gloss
 
 def get_morphs(xmlfile):
 	tree = ET.parse(corpus_dir + xmlfile)
@@ -39,26 +22,44 @@ def get_morphs(xmlfile):
 	morph_list = []
 
 	for phrase in root.findall('./body/morphemes/phrase'):
-		morph_list = morph_list + phrase.findall('morph')
+		morph_list.append(phrase.findall('morph'))
+
+	morph_list = [x for x in morph_list if x != []]
 
 	return morph_list
 
-def get_pos(word,xmlfile):
+def get_pos(morphlist,xmlfile):
 	tree = ET.parse(corpus_dir + xmlfile)
 	root = tree.getroot()
 	
-	try:
-		pos = root.find("./body/postags/phrase/pos[@morph_ref='" + word.get('morph_id') + "']").get('text')
-	except AttributeError:
-		return ['err','err']
+	pos_list = []
 
-	return [word.get('text'), pos]
+	for morph in morphlist:
+
+		try:
+			pos = root.find("./body/postags/phrase/pos[@morph_ref='" + morph.get('morph_id') + "']").get('text')
+		except AttributeError:
+			pos = 'err'
+
+		m = ''.join(list(filter(lambda ch: ch not in " +.!¡¿?-,", morph.get('text'))))
+		p = pos.upper()
+
+		pos_list.append(m + ' ' + p)
+
+	return pos_list
 
 
 
 def main():
 
-	with open("pos.csv", "w") as csvfile:
+	# for xmlfile in ['1.xml']:
+
+	# 	mor = get_morphs(xmlfile)
+	# 	for i in mor:
+	# 		pos = get_pos(i, xmlfile)
+	# 		print(pos)
+
+	with open("pos_list.csv", "w") as csvfile:
 		writer = csv.writer(csvfile)
 
 		n = 1
@@ -70,10 +71,8 @@ def main():
 			n = n + 1
 
 			for m in get_morphs(xmlfile):
-				mp = get_pos(m,xmlfile)
-				mp[0] = ''.join(list(filter(lambda ch: ch not in " +.!¡¿?-,", mp[0])))
-				mp[1] = mp[1].upper()
-				writer.writerow(mp)
+				poses = get_pos(m,xmlfile)
+				writer.writerow(poses)
 		
 if __name__ == '__main__':
-	main() 
+	main()
