fef3863aaee0dc2f78880d2dc3796992717c76e2
Robert Henderson <balamkej@gmail.com>
2020-07-16 12:19:05 -0700

    trained and tested models with logs

--- averged_perceptron/train_test.py
+++ averged_perceptron/train_test.py
@@ -9,6 +9,44 @@ data_file = '../data/clean/pos_phrase-cleaned.csv'
 
 model_file = 'usp_pos_model'
 
+
+def test_sentence(sent):
+    f = open('log.txt', 'a')
+    f.write(str(sent) + '\n\n')
+
+    START = ['-START-', '-START2-']
+    END = ['-END-', '-END2-']
+    prev, prev2 = START
+
+    words = sent[0]
+    tags = sent[1]
+    context = START + words + END
+
+    hit = 0
+
+    miss = 0
+
+    for i, word in enumerate(words):
+        feat = trainer._get_features(i, word, context, prev, prev2)
+        y = tags[i]
+        y_hat = tester.predict(feat)
+
+        f.write('morph:' + word + '\n')
+        f.write('actual:' + y + '\n')
+        f.write('predicted:' + y_hat + '\n\n')
+
+        if y == y_hat:
+            hit = hit + 1
+        else:
+            miss = miss + 1
+
+    f.close()
+
+    return [hit,miss]
+
+
+
+
 sentences = []
 
 with open(data_file, "r") as csv_file:
@@ -52,88 +90,16 @@ tester.load(model_file)
 
 print('Testing', model_file, 'on', len(test), 'test sentences.\n')
 
+print('Number of classes:', len(tester.classes), '\n')
 
+hit = 0
+miss = 0
+f = open('log.txt','w')
+f.close()
 
-# import csv
-# from collections import defaultdict
-# from averged_perceptron import AveragedPerceptron
+for sentence in test:
+    result = test_sentence(sentence)
+    hit = hit + result[0]
+    miss = miss + result[1]
 
-# file = "../data/clean/pos_phrase-cleaned.csv"
-
-# sentences = []
-
-# with open(file, "r") as csv_file:
-#     csv_reader = csv.reader(csv_file, delimiter=',')
-#     for line in csv_reader:
-#         words = []
-#         tags = []
-#         for cell in line:
-#             cells = cell.split()
-#             words.append(cells[0])
-#             tags.append(cells[1])
-#         sentences.append((words,tags))
-
-# def _get_features(i, word, context, prev, prev2):
-#     '''Map tokens into a feature representation, implemented as a
-#     {hashable: float} dict. If the features change, a new model must be
-#     trained.
-#     '''
-#     def add(name, *args):
-#         features[' '.join((name,) + tuple(args))] += 1
-
-#     i += len(START)
-#     features = defaultdict(int)
-#     # It's useful to have a constant feature, which acts sort of like a prior
-#     add('bias')
-#     add('i suffix', word[-3:])
-#     add('i pref1', word[0])
-#     add('i-1 tag', prev)
-#     add('i-2 tag', prev2)
-#     add('i tag+i-2 tag', prev, prev2)
-#     add('i word', context[i])
-#     add('i-1 tag+i word', prev, context[i])
-#     add('i-1 word', context[i-1])
-#     add('i-1 suffix', context[i-1][-3:])
-#     add('i-2 word', context[i-2])
-#     add('i+1 word', context[i+1])
-#     add('i+1 suffix', context[i+1][-3:])
-#     add('i+2 word', context[i+2])
-#     return features
-
-# START = ['-START-', '-START2-']
-# END = ['-END-', '-END2-']
-
-# prev, prev2 = START
-# # for words, tags in sentences:
-# #     context = START + words + END
-
-# #     for i, word in enumerate(words):
-# #         print(_get_features(i, word, context, prev, prev2))
-# #         prev2 = prev
-# #         prev = tags[i]
-
-
-# percep = AveragedPerceptron()
-# percep.load("usp_pos_model")
-
-
-
-# words = sentences[0][0]
-# tags = sentences[0][1]
-# context = START + words + END
-
-# # print(words)
-# # print(tags)
-
-# for i, word in enumerate(words):
-#     morphs = []
-#     morphs.append((word,_get_features(i, word, context, prev, prev2)))
-#     prev2 = prev
-#     prev = tags[i]
-
-#     for word, feat in morphs:
-#         print("morph:", word)
-#         print("prediction:", percep.predict(feat) + '\n')
-
-
-# # print(type(percep.weights[2]))
+print('Accuracy:', hit / (hit + miss))
